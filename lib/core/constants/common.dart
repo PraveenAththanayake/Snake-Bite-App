d760ef47ab3a101ebb9fbe8ac67b3162d28f0bf5
Praveen Aththanayake <aththanayakempa@gmail.com>
2024-10-23 12:02:10 +0530

    feat: created constants

---
+++ lib/core/constants/common.dart
@@ -0,0 +1,3 @@
+class Sizes {
+  static const double radius = 0.75;
+}
---
+++ lib/core/constants/text_string.dart
@@ -0,0 +1,14 @@
+class THomeScreenStrings {
+  static const String upload = "Capture Bite Image";
+  static const String uploadDesc = "Ensure the bite is visible and well lit";
+  static const String uploadButton = "Upload Image";
+}
+
+class TUploadScreenStrings {
+  static const String title = "Capture the Identified Area";
+  static const String upload = "Capture the Bite Area";
+  static const String uploadDesc = "Good lighting improves accuracy";
+  static const String uploadSpanDesc = "Upload Image";
+  static const String uploadButton = "Upload Image";
+  static const String label = "Nearest Hospital";
+}
